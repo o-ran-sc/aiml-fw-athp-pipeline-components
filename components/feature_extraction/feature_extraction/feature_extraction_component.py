9567b6fde321a2dc6b6b2c107afb43eb54fcd7bd
subhash kumar singh <subh.singh@samsung.com>
2025-05-20 12:41:38 +0000

    Component structure for feature extraction
    Structure for `feature-extraction` component.
    
    Issue-ID: AIMLFW-192
    Change-Id: If41a09166314584ff88132598d3cb6a72c55e354
    Signed-off-by: subhash kumar singh <subh.singh@samsung.com>

---
+++ components/feature_extraction/feature_extraction/feature_extraction_component.py
@@ -0,0 +1,32 @@
+# ==================================================================================
+#
+#       Copyright (c) 2025 Samsung Electronics Co., Ltd. All Rights Reserved.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ==================================================================================
+from kfp.dsl import component
+from typing import List
+
+@component(
+    base_image="python:3.12",
+    packages_to_install=[],
+    target_image="feature_extraction:v1",
+    pip_index_urls=["https://pypi.org/simple/"],
+)
+def download_features(featurepath: str, featureList: List[str])->str:
+    import logging
+    logger = logging.getLogger('download_features')
+    logger.setLevel(logging.INFO)
+    logger.info(f'donwload feature from path:{featurepath} featurelist:{featureList}')
+    return ""
