256334c9b9f05d5272520a152d777a660e7be284
subhash kumar singh <subh.singh@samsung.com>
2025-06-20 09:38:10 +0000

    Implemente model storage component
    Provided implementation for model storage.
    
    Issue-ID: AIMLFW-196
    Change-Id: I94c505ad6f02c3b3d1b5c9dfa1171dd074c3354a
    Signed-off-by: subhash kumar singh <subh.singh@samsung.com>

--- components/model_storage/model_storage/model_storage_component.py
+++ components/model_storage/model_storage/model_storage_component.py
@@ -19,12 +19,43 @@ from kfp.dsl import component
 
 @component(
     base_image="python:3.12",
-    packages_to_install=[],
+    packages_to_install=["modelmetricsdk", "requests==2.28.0"],
     target_image="model_storage:v1",
     pip_index_urls=["https://pypi.org/simple/"],
 )
-def model_storage(modelpath: str)->str:
+def model_storage(modelpath: str,
+                  modelname: str,
+                  modelversion:str)->str:
     from logger import get_default_logger
+    import requests
+    from modelmetricsdk.model_metrics_sdk import ModelMetricsSdk
+
     logger = get_default_logger(name='model-storage')
     logger.info(f'model storage will be done to modelpath:{modelpath}')
+
+    # store model
+    print(f'register the trained model to mme')
+    artifactversion="1.0.0"    # need to decide if it is right to provide 1.0.0
+    url = f"http://modelmgmtservice.traininghost:8082/ai-ml-model-registration/v1/model-registrations/updateArtifact/{modelname}/{modelversion}/{artifactversion}"
+
+    print(f'sending request to URL :{url}')
+    try:
+        updated_model_info= requests.post(url).json()
+        print(updated_model_info)
+    except Exception as e:
+        print(f'error while mme operation: {e}')
+        raise e
+
+    print(f'uploading model to leofs with name:{modelname}, modelver:{modelversion}, artifactnumber:{artifactversion}')
+    mmsdk = ModelMetricsSdk()
+
+    try:
+        mmsdk.upload_model(model_path="./", model_name=modelname,
+                           model_version=modelversion,
+                           artifact_version=artifactversion)
+    except Exception as e:
+        print(f'error while uploading the model:{e}')
+        raise e
+
+    print('successfully uploaded the model')
     return "done"
